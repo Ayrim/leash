982d5ab2f408f63f9f0d0a831be72d40ac4c7d4c
Maxim Braekman <maxim.braekman@hotmail.com>
2016-01-26 21:11:16 +0100

    Adding some 'style'
    Adding a home-page + some styling to start from.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  #before_action :require_login, except: [:index, :new]
+  before_action :require_login, except: [ :new]
   #before_create :check_valid_email
 
 	def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   resources :users
   resources :user_sessions, only: [ :new, :create, :destroy ]
+  resources :home
 
   get 'login'   => 'user_sessions#new', :as => :login
   get 'logout'  => 'user_sessions#destroy', :as => :logout
+  get 'signup'  => 'users#new', :as => :signup
+  get 'home'    => 'home#index', :as => :home_root
   
-  root 'users#index'
+  #root 'users#index'
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
