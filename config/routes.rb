2eb33ff2c98e5ce9b45753c182254603a77b6ad0
Ayrton Vercruysse <ayrton.vercruysse@gmail.com>
2016-02-06 17:05:50 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :user_sessions, only: [ :new, :create, :destroy ]
   resources :home
   resources :account_activations, only: [:edit]
+  resources :animals
 
   get 'login'   => 'user_sessions#new', :as => :login
   get 'logout'  => 'user_sessions#destroy', :as => :logout
