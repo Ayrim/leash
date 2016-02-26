d261bdde5e916d29ee309ecb9b9b81be90d5de0e
Maxim Braekman <maxim.braekman@hotmail.com>
2016-02-26 12:41:22 +0100

    structure + toast

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -9,4 +9,16 @@
 			  $('#disagreeTermsBtn').click(function(){
 				document.getElementById("agree-terms").checked = false;
 			  });
+			  
+			/*if (window.location.pathname.endsWith('login.html')){*/
+				if (window.location.hash === '#activation') {ActivationToast();}
+				if (window.location.hash === '#password') {PasswordToast();}
+			/*}*/
 		  });
+		  
+        function ActivationToast () {
+			Materialize.toast('Thank you for registering!<br /><br />An activation mail has been sent,<br />which can be used to activate your account.', 7000);
+        }
+        function PasswordToast () {
+			Materialize.toast('An e-mail containing your password has been sent.', 7000);
+        }
