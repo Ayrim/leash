12c6e430065f50973a0263f5af841d696da23f42
Maxim Braekman <maxim.braekman@hotmail.com>
2016-03-02 22:28:01 +0100

    Add Edit page

---
+++ scripts/settings.js
@@ -0,0 +1,9 @@
+/*$(document).ready(function(){*/
+	$('.scrollspy').scrollSpy();
+/*});*/
+
+$('.datepicker').pickadate({
+	selectMonths: true, // Creates a dropdown to control month
+	selectYears: 75, // Creates a dropdown of 15 years to control year
+	max: true
+});
