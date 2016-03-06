a5e7b657ebe10da74846a7dd261ca712ceaa2367
Maxim Braekman <maxim.braekman@hotmail.com>
2016-03-06 15:51:27 +0100

    Update Edit-page

--- scripts/EditSettings.js
+++ scripts/EditSettings.js
@@ -1,9 +1,16 @@
-/*$(document).ready(function(){*/
+$(document).ready(function(){
+	$(".button-collapse").sideNav();
+	$(".brand-logo").sideNav();
 	$('.scrollspy').scrollSpy();
-/*});*/
+})
+		
 
 $('.datepicker').pickadate({
 	selectMonths: true, // Creates a dropdown to control month
 	selectYears: 75, // Creates a dropdown of 15 years to control year
 	max: true
+});
+
+$(window).scroll(function(){
+    $(".pinned").css("top",Math.max(75,450-$(this).scrollTop()));
 });
