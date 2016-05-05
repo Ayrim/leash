06cff23ced8a04b2e54e9884fe41d6e9fe5c261b
Maxim Braekman <maxim.braekman@hotmail.com>
2016-05-05 17:22:39 +0200

    Post loading

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,12 @@ class HomeController < ApplicationController
 
 	def overview
 		@hideHome = true;
+
+		# get the 10 most recent wallposts which contain a picture
+		# userIdQuery = "user_id != " + current_user.id.to_s
+		# posts = Wallpost.order(created_at: :desc).where(userIdQuery).where("picture is not NULL").limit(10)
+		posts = Wallpost.order(created_at: :desc).where("picture is not NULL").limit(10)
+		# take 3 random wallposts from these 10
+		@PicturePosts = posts.shuffle.take(3)
 	end
 end
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,5 @@
 module HomeHelper
+	def collectWallpostPicture(pictureId)
+		return Picture.find_by(:id => pictureId).url;
+	end
 end
