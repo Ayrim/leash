bbdbe4cbd275473ffb48fc74c153abf8308c7048
Maxim Braekman <maxim.braekman@hotmail.com>
2016-05-27 20:46:22 +0200

    Multi-language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   $title = "leash"
+
+  before_action :set_locale
+  def set_locale
+  	if(params[:locale])
+  		session[:locale] = params[:locale]
+  	end
+    I18n.locale = params[:locale] || session[:locale] || I18n.default_locale
+
+  end
 end
