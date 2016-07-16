5577368f9e24b275d1f73ed48e82d9390b11acca
Maxim Braekman <maxim.braekman@hotmail.com>
2016-07-16 17:00:58 +0200

    Update DB-schema

--- db/schema.rb
+++ db/schema.rb
@@ -103,8 +103,11 @@ ActiveRecord::Schema.define(version: 20160715184313) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "connections", id: false, force: :cascade do |t|
-    t.integer "user_id"
+  create_table "connections", force: :cascade do |t|
+    t.integer  "user_1_id"
+    t.integer  "user_2_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "connections", ["user_id", "user_id"], name: "index_connections_on_user_id_and_user_id", using: :btree
